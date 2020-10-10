9e2df39339298a8c35f8a52e74bd071dd67aeb5d
nuruddin <nuruddinhossian47@gmail.com>
2020-10-10 14:52:19 +0600

    first commit

---
+++ assets/js/active.js
@@ -0,0 +1,25 @@
+(function ($) {
+    "use strict";
+
+    jQuery(document).ready(function ($) {
+        
+        $('.welcome-slider-wrap-class').owlCarousel({
+            items: 1,
+            margin: 30,
+            loop: false,
+            dots: false,
+            nav: false,
+            navText: ["<i class='fa fa-long-arrow-left'></i>", "<i class='fa fa-long-arrow-right'></i>"],
+            autoplay: false,
+            smartSpeed: 500,
+            animateOut: "fadeOut"
+        });
+        
+    });
+
+
+    jQuery(window).on("load", function () {
+
+    });
+
+})(jQuery);
